fb05092de8f7284d26ce68abc2e3a8c893a22ad9
AabaJaba <85494789+AabaJaba@users.noreply.github.com>
2022-07-05 14:55:10 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -1,23 +1,19 @@
-var=[];lab={};hltcount=0;varcount=0;regop={"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110","FLAGS":"111"}
-with open("CO project/test.txt","r") as f:#open txt file
-    inst=[x.rstrip("\n").split() for x in f.readlines()]
-inst=[x for x in inst if x!=[]] #remove empty lines
-assert len(inst)<=256, "Instruction input can't be greater than 256 lines" #greater than 256 lines input
+var=[];lab={};hltcount=0;varcount=0;actualcount=0;regop={"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110","FLAGS":"111"};instructions=["var","add","sub","mov","ld","st","mul","div","rs","ls","xor","or","and","not","cmp","jmp","jgt","je","addf","subf","movf","hlt","FLAGS"]
+f=open("CO project/test.txt","r")#open txt file
+inst=[x.rstrip("\n").split() for x in f.readlines()]
 flgchck=list(regop.keys())
 flgchck.remove('FLAGS') 
-for x in inst:
-    if 'FLAGS' in x:
-        assert x[0]=='mov' and x[1] in flgchck and x[2]=='FLAGS', f'Error in line {inst.index(x)+1}: Invalid FLAGS register use'#invalid flag use
+print(inst)
 def typea(i):
     assert len(i)==4, f'Error in line {inst.index(i)+1}: This instruction must contain 3 arguments, not {len(i)-1}'# invalid instruction length 
-    assert i[1] in regop.keys() and i[2] in regop.keys() and i[3] in regop.keys(), 'Enter Valid register values'
+    assert i[1] in regop.keys() and i[2] in regop.keys() and i[3] in regop.keys(), f'Error in line {inst.index(i)+1}: Enter Valid register values'
     i[1]=regop[i[1]]
     i[2]=regop[i[2]]
     i[3]=regop[i[3]]         
     i.insert(1,'00')
 def typeb(i):
     assert len(i)==3, f'Error in line {inst.index(i)+1}: This instruction must contain 2 arguments, not {len(i)-1}'# invalid instruction length 
-    assert i[1] in regop.keys(), 'Enter valid register values'
+    assert i[1] in regop.keys(), f'Error in line {inst.index(i)+1}: Enter valid register values'
     i[1]=regop[i[1]]
     i[2]=i[2].lstrip('$')
     try:
@@ -30,39 +26,48 @@ def typeb(i):
     i[2]=('0'*(8-len(i[2]))) + i[2]
 def typec(i):
     assert len(i)==3, f'Error in line {inst.index(i)+1}: This instruction must contain 2 arguments, not {len(i)-1}'#invalid instruction length
-    assert i[1] in regop.keys() and i[2] in regop.keys(), 'Enter Valid register values'#invalid register values
+    assert i[1] in regop.keys() and i[2] in regop.keys(), f'Error in line {inst.index(i)+1}: Enter Valid register values'#invalid register values
     i[1]=regop[i[1]]
     i[2]=regop[i[2]]
     i.insert(1,'00000')
 def typed(i):
     assert len(i)==3, f'Error in line {inst.index(i)+1}: This instruction must contain 2 arguments, not {len(i)-1}'#invalid instruction length
-    assert i[1] in regop.keys(), 'Enter Valid Register Values'
+    assert i[1] in regop.keys(), f'Error in line {inst.index(i)+1}: Enter Valid Register Values'
     i[1]=regop[i[1]]
     assert i[2] not in lab.keys(), f'Error in line {inst.index(i)+1}: Use of labels in place of variables is forbidden'#label instead of variable
     assert i[2] in var, f'Error in line {inst.index(i)+1}: "{i[2]}" has not been defined'#var not declared
-    i[2]=bin(int((len(inst)-len(var))+(var.index(i[2]))))[2:]
+    i[2]=bin(int((actualcount-len(var))+(var.index(i[2]))))[2:]
     i[2]=('0'*(8-len(i[2]))) + i[2]
 def typee(i):
     assert len(i)==2, f'Error in line {inst.index(i)+1}: This instruction must contain 1 arguments, not {len(i)-1}'#invalid instruction length
-    assert i[1] not in var, f'Error in line {inst.index(i)+1}: Use of labels in place of variables is forbidden'#variable instead of label
+    assert i[1] not in var, f'Error in line {inst.index(i)+1}: Use of variables in place of labels is forbidden'#variable instead of label
     assert i[1] in lab.keys(), f'Error in line {inst.index(i)+1}: "{i[1]}" has not been defined'#label not declared
     i[1]=bin(lab[i[1]])[2:]
     i[1]=('0'*(11-len(i[1]))) + i[1] 
-for i in range(len((inst))):#labels
-    if inst[i][0][-1]==":":
-        inst[i][0]=inst[i][0].rstrip(":")
-        assert inst[i][0].isalnum() and not inst[i][0][0].isnumeric(), f'Error in line {i}: Invalid label name' #invalid label
-        lab[inst[i].pop(0)]=i
-        if len(inst[i])==0:#empty label
-            inst[i].append("xx")
 for i in range(len((inst))):
+    if inst[i]==[]:
+        varcount+=1
+        continue
+    else:
+        actualcount+=1
     if inst[i][0]=='var':
         assert len(inst[i])==2, f"Error in line {i}:This instruction can't contain more than one argument" #more than 1 argument 
+        assert inst[i][1] not in instructions, f'Error in line{i}: "{inst[i][0]}" is an invalid var name, var names can not be same as instructions' #var name can't be same as instruction name
         assert inst[i][1].isalnum() and not inst[i][1][0].isnumeric(), f'Error in line {i}: Invalid variable name' #invalid variable
         assert varcount==i, "All variables must be defined at the start of the code"#var defined between code
         assert inst[i][1] not in var, f"Error in line {i}: Can't redefine a variable"#var redefine
         var.append(inst[i][1])
         varcount+=1
+    if inst[i][0][-1]==":":#lables
+        assert len(inst[i])>1, f'Error in line {i}: label is empty'#empty label error 
+        inst[i][0]=inst[i][0].rstrip(":")
+        assert inst[i][0] not in instructions, f'Error in line {i}: "{inst[i][0]}" is an invalid label name, label names can not be same as instructions' #label name can't be same as instruction name
+        assert inst[i][0].isalnum() and not inst[i][0][0].isnumeric(), f'Error in line {i}: Invalid label name' #invalid label
+        assert inst[i][0] not in lab.keys(), f"Error in line {i}: Can't redefine a label"#label redefine 
+        assert inst[i][0] not in var, f"Error in line{i}: Can't use variable nae as label name" #var name as label 
+        lab[inst[i].pop(0)]=actualcount
+    if 'FLAGS' in inst[i]:#flags
+        assert inst[i][0]=='mov' and inst[i][2] in flgchck and inst[i][1]=='FLAGS', f'Error in line {i}: Invalid use of FLAGS register'#invalid flag use
     elif inst[i][0]=='add':
         inst[i][0]='10000'
         typea(inst[i])
@@ -130,16 +135,39 @@ for i in range(len((inst))):
         inst[i][0]=='00010'
         typeb(inst[i])
     elif inst[i][0]=='hlt':   
-        assert i==len(inst)-1, f'Error in line {i}:"hlt" must be the last instruction'
+        assert i==actualcount-1, f'Error in line {i}:"hlt" must be the last instruction'
         assert hltcount==0, f'Error in line {i}:"hlt" must be used only once'
         hltcount+=1
         inst[i][0]='0101000000000000'
-    elif inst[i][0]=='xx':
-        inst[i][0]="0000000000000000"
     else:
         assert False, f'Error in line {i}: Syntax error, "{"".join(inst[i])}" is not a valid argument'#invalid instruction
 assert hltcount!=0, '"hlt" instruction is absent'
+inst=[x for x in inst if x!=[]]
+assert actualcount<=256, "Instruction input can't be greater than 256 lines" #greater than 256 lines input
 inst=[x for x in inst if x[0]!='var']#removing var from list
 inst=["".join(x)+"\n" for x in inst]
-result=open('CO project/result.txt','w')
-result.writelines(inst)
+for x in inst:
+    print(*x,sep='')
+
+
+
+
+
+    
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
